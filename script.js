5d5975d7934e30a1da19999d67f9c2b644fa3d46
Olena Koppalova <64489905+lenhack@users.noreply.github.com>
2022-03-23 12:52:58 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const toggleBtn = document.querySelector('.toggle-btn');
-const menubox = document.querySelector('.menubox');
-
-function activateMenu(){
-    toggleBtn.classList.toggle('active');
-    menubox.classList.toggle('active');
-}
-toggleBtn.addEventListener('click', activateMenu);
